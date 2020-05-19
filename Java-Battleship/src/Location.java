c9642ce6adc11bb05b6a16d330d8cdbf73348706
Julen Etxaniz <juletxara@gmail.com>
2020-05-19 18:47:28 +0200

    Java proiektuko kodea formateatu

--- Java-Battleship/src/Location.java
+++ Java-Battleship/src/Location.java
@@ -1,106 +1,91 @@
-public class Location
-{
-    // Global Vars
-    public static final int UNGUESSED = 0;
-    public static final int HIT = 1;
-    public static final int MISSED = 2;
-    
-    // Instance Variables
-    private boolean hasShip;
-    private int status;
-    private int lengthOfShip;
-    private int directionOfShip;
-    
-    // Location constructor. 
-    public Location()
-    {
-        // Set initial values
-        status = 0;
-        hasShip = false;
-        lengthOfShip = -1;
-        directionOfShip = -1;
-    }
+public class Location {
+	// Global Vars
+	public static final int UNGUESSED = 0;
+	public static final int HIT = 1;
+	public static final int MISSED = 2;
 
-    // Was this Location a hit?
-    public boolean checkHit()
-    {
-        if (status == HIT)
-            return true;
-        else
-            return false;
-    }
+	// Instance Variables
+	private boolean hasShip;
+	private int status;
+	private int lengthOfShip;
+	private int directionOfShip;
 
-    // Was this location a miss?
-    public boolean checkMiss()
-    {
-        if (status == MISSED)
-            return true;
-        else
-            return false;
-    }
+	// Location constructor.
+	public Location() {
+		// Set initial values
+		status = 0;
+		hasShip = false;
+		lengthOfShip = -1;
+		directionOfShip = -1;
+	}
 
-    // Was this location unguessed?
-    public boolean isUnguessed()
-    {
-        if (status == UNGUESSED)
-            return true;
-        else
-            return false;
-    }
+	// Was this Location a hit?
+	public boolean checkHit() {
+		if (status == HIT)
+			return true;
+		else
+			return false;
+	}
 
-    // Mark this location a hit.
-    public void markHit()
-    {
-        setStatus(HIT);
-    }
+	// Was this location a miss?
+	public boolean checkMiss() {
+		if (status == MISSED)
+			return true;
+		else
+			return false;
+	}
 
-    // Mark this location a miss.
-    public void markMiss()
-    {
-        setStatus(MISSED);
-    }
+	// Was this location unguessed?
+	public boolean isUnguessed() {
+		if (status == UNGUESSED)
+			return true;
+		else
+			return false;
+	}
 
-    // Return whether or not this location has a ship.
-    public boolean hasShip()
-    {
-        return hasShip;
-    }
+	// Mark this location a hit.
+	public void markHit() {
+		setStatus(HIT);
+	}
 
-    // Set the value of whether this location has a ship.
-    public void setShip(boolean val)
-    {
-        this.hasShip = val;
-    }
+	// Mark this location a miss.
+	public void markMiss() {
+		setStatus(MISSED);
+	}
 
-    // Set the status of this Location.
-    public void setStatus(int status)
-    {
-        this.status = status;
-    }
+	// Return whether or not this location has a ship.
+	public boolean hasShip() {
+		return hasShip;
+	}
 
-    // Get the status of this Location.
-    public int getStatus()
-    {
-        return status;
-    }
-    
-    public int getLengthOfShip()
-    {
-        return lengthOfShip; 
-    }
-    
-    public void setLengthOfShip(int val)
-    {
-        lengthOfShip = val;
-    }
-    
-    public int getDirectionOfShip()
-    {
-        return directionOfShip; 
-    }
-    
-    public void setDirectionOfShip(int val)
-    {
-        directionOfShip = val;
-    }
+	// Set the value of whether this location has a ship.
+	public void setShip(boolean val) {
+		this.hasShip = val;
+	}
+
+	// Set the status of this Location.
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	// Get the status of this Location.
+	public int getStatus() {
+		return status;
+	}
+
+	public int getLengthOfShip() {
+		return lengthOfShip;
+	}
+
+	public void setLengthOfShip(int val) {
+		lengthOfShip = val;
+	}
+
+	public int getDirectionOfShip() {
+		return directionOfShip;
+	}
+
+	public void setDirectionOfShip(int val) {
+		directionOfShip = val;
+	}
 }
--- Java-Battleship/src/Randomizer.java
+++ Java-Battleship/src/Randomizer.java
@@ -1,49 +1,48 @@
+
 // This specific Java code was given by CodeHS, to help with the random number aspect of the program
 import java.util.*;
 
-public class Randomizer
-{
+public class Randomizer {
 
- public static Random theInstance = null;
- 
- public Randomizer(){
-  
- }
- 
- public static Random getInstance(){
-  if(theInstance == null){
-   theInstance = new Random();
-  }
-  return theInstance;
- }
- 
- public static boolean nextBoolean(){
-  return Randomizer.getInstance().nextBoolean();
- }
+	public static Random theInstance = null;
 
- public static boolean nextBoolean(double probability){
-  return Randomizer.nextDouble() < probability;
- }
- 
- public static int nextInt(){
-  return Randomizer.getInstance().nextInt();
- }
+	public Randomizer() {
 
- public static int nextInt(int n){
-  return Randomizer.getInstance().nextInt(n);
- }
+	}
 
- public static int nextInt(int min, int max){
-  return min + Randomizer.nextInt(max - min + 1);
- }
+	public static Random getInstance() {
+		if (theInstance == null) {
+			theInstance = new Random();
+		}
+		return theInstance;
+	}
 
- public static double nextDouble(){
-  return Randomizer.getInstance().nextDouble();
- }
+	public static boolean nextBoolean() {
+		return Randomizer.getInstance().nextBoolean();
+	}
 
- public static double nextDouble(double min, double max){
-  return min + (max - min) * Randomizer.nextDouble();
- }
+	public static boolean nextBoolean(double probability) {
+		return Randomizer.nextDouble() < probability;
+	}
 
- 
+	public static int nextInt() {
+		return Randomizer.getInstance().nextInt();
+	}
+
+	public static int nextInt(int n) {
+		return Randomizer.getInstance().nextInt(n);
+	}
+
+	public static int nextInt(int min, int max) {
+		return min + Randomizer.nextInt(max - min + 1);
+	}
+
+	public static double nextDouble() {
+		return Randomizer.getInstance().nextDouble();
+	}
+
+	public static double nextDouble(double min, double max) {
+		return min + (max - min) * Randomizer.nextDouble();
+	}
+
 }
